b270dc6a1d46f9a4caa74989fb77815a980934e5
Alberto <alberto@aramirez.es>
2015-06-16 23:00:47 +0200

    Add example1

--- src/main/java/example1/ObservableCreate.java
+++ src/main/java/example1/ObservableCreate.java
@@ -1,10 +1,41 @@
 package example1;
 
 import base.ExecutableExample;
+import rx.Observable;
+import rx.Subscriber;
 
 public class ObservableCreate implements ExecutableExample
 {
     public void execute() {
-        System.out.println("Hello1");
+        Observable.create(new Observable.OnSubscribe<Integer>() {
+            @Override
+            public void call(Subscriber<? super Integer> observer) {
+                try {
+                    if (!observer.isUnsubscribed()) {
+                        for (int i = 1; i < 5; i++) {
+                            observer.onNext(i);
+                        }
+                        observer.onCompleted();
+                    }
+                } catch (Exception e) {
+                    observer.onError(e);
+                }
+            }
+        } ).subscribe(new Subscriber<Integer>() {
+            @Override
+            public void onNext(Integer item) {
+                System.out.println("Next: " + item);
+            }
+
+            @Override
+            public void onError(Throwable error) {
+                System.err.println("Error: " + error.getMessage());
+            }
+
+            @Override
+            public void onCompleted() {
+                System.out.println("Sequence complete.");
+            }
+        });
     }
 }
